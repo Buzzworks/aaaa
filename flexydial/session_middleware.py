abbb6c08907bb2aa4c86c99dd46b496c9a015851
manojkumargundeti <manoj.gundeti@flexydial.com>
2022-07-27 09:55:21 +0000

    Session hijacking fixed

--- flexydial/session_middleware.py
+++ flexydial/session_middleware.py
@@ -12,18 +12,14 @@ class SessionIdleMiddleware:
 		self.get_response = get_response
 
 	def __call__(self, request):
-		x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-		if x_forwarded_for:
-			ip = x_forwarded_for.split(',')[0]
-		else:
-			ip = request.META.get('REMOTE_ADDR')
+		csrf_cookie = request.META.get('CSRF_COOKIE')
 		if request.user.is_authenticated:
-			# if 'ip_address' not in request.session:
-			# 	request.session['ip_address'] = ip
-			# else:
-			# 	if request.session['ip_address'] != ip:
-			# 		# print("SecurityError :: Using Unautherized way to access the account by... "+ip+" to the user " + request.user + " to ip " + request.session['ip_address'])
-			# 		return render(request, '404.html')
+			if 'usr_csrf_cookie' not in request.session:
+				request.session['usr_csrf_cookie'] = csrf_cookie
+			else:
+				if request.session['usr_csrf_cookie'] != csrf_cookie:
+					# print("SecurityError :: Using Unautherized way to access the account by... "+ip+" to the user " + request.user + " to ip " + request.session['ip_address'])
+					return render(request, '404.html')
 			if 'last_request' in request.session:
 				elapsed = time.time() - request.session['last_request']
 				if elapsed > settings.SESSION_IDLE_TIMEOUT:
