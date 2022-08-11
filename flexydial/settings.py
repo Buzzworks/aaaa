d08e6e4e1b989d191a2418e9110e439c2f9a6bb6
manojkumargundeti <68153329+manojkumargundeti@users.noreply.github.com>
2022-08-11 17:03:37 +0530

    settings file name changed.

--- flexydial/settings.py
+++ flexydial/settings.py
@@ -319,7 +319,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
+            'filename': 'flexydial-app.log',
             'formatter': 'verbose'
         },
     },
